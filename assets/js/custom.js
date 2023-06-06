ee976b77722a5c14debe0f948ee00b949d2bba74
kau-nakrani <knakrani100@gmail.com>
2023-06-06 06:13:52 +0530

    update new code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -16,13 +16,89 @@ $(".count-number").each(function () {
     );
 });
 // Counting number js end
-// ------------------------ CALENDAR JS START ----------------------------
-$(document).ready(function () {
-  $(".fc-button-group button").click(function () {
-    $(".fc-left").removeClass("open-week-time");
-  })
-  $(".fc-agendaWeek-button").click(function () {
-    $(".fc-left").addClass("open-week-time");
-  })
-})
-// ------------------------ CALENDAR JS END ----------------------------
+// SIDEBAR JS START
+const contentBody = document.querySelector(".wd-body");
+const sidebar = document.querySelector(".wd-sidebar");
+const toggleButton = document.querySelector(".wd-sidebar-toggler-button");
+const toggleButtonMobile = document.querySelector(
+  ".wd-mobile-sidebar-toggler-button"
+);
+const closeButtonMobile = document.querySelector(".wd-mobile-menu-button");
+const mainBody = document.querySelector(".wd-main");
+
+toggleButton.addEventListener("click", function () {
+  contentBody.classList.toggle("wd-sidebar-collapsed");
+});
+
+toggleButtonMobile.addEventListener("click", function () {
+  contentBody.classList.remove("wd-sidebar-collapsed");
+  contentBody.classList.toggle("wd-sidebar-slides-in");
+  mainBody.classList.add("overflow-hidden");
+});
+
+closeButtonMobile.addEventListener("click", function () {
+  if (contentBody.classList.contains("wd-sidebar-slides-in")) {
+    contentBody.classList.remove("wd-sidebar-slides-in");
+    mainBody.classList.remove("overflow-hidden");
+  } else {
+  }
+});
+//  SIDEBAR JS END
+// TEXT EDITOR JS START
+var richTextEditor = {
+  bindEvents: function () {
+    this.bindDesignModeToggle();
+    this.bindToolbarButtons();
+  },
+
+  bindDesignModeToggle: function () {
+    $("#page").on("click", function (e) {
+      document.designMode = "on";
+    });
+
+    $(document.body).on("click", function (e) {
+      var $target = $(e.target);
+
+      if ($target.is("body")) {
+        document.designMode = "off";
+      }
+    });
+  },
+
+  bindToolbarButtons: function () {
+    $("#toolbar").on(
+      "mousedown",
+      ".icon",
+      function (e) {
+        e.preventDefault();
+        var btnId = $(e.target).attr("id");
+        this.editStyle(btnId);
+      }.bind(this)
+    );
+  },
+
+  editStyle: function (btnId) {
+    var value = null;
+
+    if (btnId === "createLink") {
+      if (this.isSelection()) value = prompt("Enter a link:");
+    }
+
+    document.execCommand(btnId, true, value);
+  },
+
+  isSelection: function () {
+    var selection = window.getSelection();
+    return selection.anchorOffset !== selection.focusOffset;
+  },
+
+  init: function () {
+    this.bindEvents();
+  },
+};
+
+richTextEditor.init();
+// TEXT EDITOR JS END
+// STEP WIZARD JS START
+
+// STEP WIZARD JS END
